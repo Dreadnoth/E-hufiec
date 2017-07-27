ce6cc79dfb8659f84abb5a1e5b01adfe28c23d57
dreadnoth <lukasz.czyrny@gmail.com>
2017-07-27 22:17:19 +0200

    DB - zbiorka repository UI - front, tabelka ze zbiórkami

--- src/Zhp/BackendBundle/Repository/ZbiorkaRepository.php
+++ src/Zhp/BackendBundle/Repository/ZbiorkaRepository.php
@@ -10,8 +10,17 @@ namespace Zhp\BackendBundle\Repository;
 
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class ZbiorkaRepository extends EntityRepository
 {
+    public function findAllForPage($page, $itemsPerPage = 10) {
+        $em = $this->getEntityManager();
 
+        $query = $em->createQuery("SELECT z FROM ZhpBackendBundle:Zbiorka z")
+            ->setFirstResult($page * ($itemsPerPage - 1))
+            ->setMaxResults($itemsPerPage);
+
+        return new Paginator($query);
+    }
 }
